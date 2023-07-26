8933e192b96158e1b8e2b3d9a1683a098c4e4ab1
mandasilemon <110959957+mandasilemon@users.noreply.github.com>
2023-07-27 05:46:33 +0700

    Add files via upload

---
+++ OperatorLogikaLKS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.operatorlogikalks;
+
+public class OperatorLogikaLKS {
+
+    public static void main(String[] args) {
+        int operand1 = 48;
+        int operand2 = 70;
+        boolean test1 =operand1> 20 &operand2<=100;
+        System.out.println(test1);
+        boolean test2 =operand1 <20 & operand2<=100;
+        System.out.println(test2);
+        
+    }
+}
